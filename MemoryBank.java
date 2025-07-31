2ee1bd2fed38be97e5001c46a45092779d514984
yassinexng <yassine.mouadi.bns@gmail.com>
2025-07-31 19:16:17 +0100

    MemoryBank.java

--- MemoryBank.java
+++ MemoryBank.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+//The memorybank's role is quite simple: we store the weights and bias of the best performing parameter sets, during the training set.
+
 public class MemoryBank {
     private final LinkedList<MemoryQuantumState> memories = new LinkedList<>();
     private final int capacity;
@@ -9,10 +11,15 @@ public class MemoryBank {
     }
 
     public void add(double[] params, double cost) {
-        double amplitude = 1.0 / (cost + 1e-8);  // better cost = higher amplitude, since it means the error is lower
+        double amplitude = 1.0 / (cost + 1e-8); 
+        //1e-8 is a very small number added to the denominator just in case the cost is exactly 0
+        // a lower cost (a better solution) should have a higher influence( a higher amplitude)
+        
         memories.addFirst(new MemoryQuantumState(params, amplitude));
+        //we simply bundle 'params' and its calculated amplitude in the new 'MemoryQuantumState' object
         if (memories.size() > capacity) {
             memories.removeLast();
+            //Checking on the list's size to see if it's overgrown its original maximum size. if it did we remove the end of the list containing the oldest state
         }
     }
 
